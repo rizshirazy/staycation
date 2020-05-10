66fb1f093bab6b47777a405dc96b65131fdb20cb
Muhammad Rizki <rizkielshirazy@gmail.com>
2020-05-10 13:08:01 +0700

    Create Header component and merge to Landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'assets/scss/styles.scss';
+import LandingPage from 'pages/LandingPage';
 
 function App() {
-	return <div className='container'>Hello World</div>;
+	return (
+		<Router>
+			<Route path='/' component={LandingPage}></Route>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/partials/IconText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from 'elements/Button';
+
+const IconText = (props) => {
+	return (
+		<Button className='brand-text-icon' href='' type='link'>
+			Stay<span className='text-gray-900'>cation.</span>
+		</Button>
+	);
+};
+
+export default IconText;
