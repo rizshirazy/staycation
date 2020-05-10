5f127194b9deff13ec704aec3960e640ff72d03c
Muhammad Rizki <rizkielshirazy@gmail.com>
2020-05-10 20:39:02 +0700

    Create Hero component and merge to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Header from 'partials/Header';
+import Hero from 'partials/Hero';
+
+import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
 	render() {
 		return (
 			<>
 				<Header {...this.props} />
+				<Hero data={landingPage.hero} />
 			</>
 		);
 	}
