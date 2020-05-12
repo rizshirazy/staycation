954c52a61786e5437f542de3ce9951913bfe94da
Muhammad Rizki <rizkielshirazy@gmail.com>
2020-05-12 12:00:55 +0700

    Create most-picked section and merge to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 import Header from 'partials/Header';
 import Hero from 'partials/Hero';
+import MostPicked from 'partials/MostPicked';
 
 import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
+	constructor(props) {
+		super(props);
+		this.refMostPicked = React.createRef();
+		this.state = {};
+	}
+
 	render() {
 		return (
 			<>
 				<Header {...this.props} />
-				<Hero data={landingPage.hero} />
+				<Hero
+					refMostPicked={this.refMostPicked}
+					data={landingPage.hero}
+				/>
+				<MostPicked
+					refMostPicked={this.refMostPicked}
+					data={landingPage.mostPicked}
+				/>
 			</>
 		);
 	}
